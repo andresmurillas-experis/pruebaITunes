0d5da8346b700b70ebf3fb5df757848b615d9ac5
andres murillas <andres.murillas@ciberexperis.es>
2023-02-23 09:11:06 +0100

    fixed according to comments by reviewer

--- PruebaItunes/ViewController.swift
+++ PruebaItunes/ViewController.swift
@@ -8,20 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    let artistListViewController = ArtistListViewController(nibName: "ArtistListViewController", bundle: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    
     @IBAction func showArtistListView(_ sender: Any) {
-        
+        let artistListViewController = ArtistListViewController(nibName: "ArtistListViewController", bundle: nil)
         navigationController?.pushViewController(artistListViewController, animated: true)
     }
-    
 
 }
 
