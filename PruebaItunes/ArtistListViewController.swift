2a4ba01d24d7dd317b6528dd362c3baf3cb278d8
andres murillas <andres.murillas@ciberexperis.es>
2023-02-23 09:01:02 +0100

    created artist list view with navigation button

---
+++ PruebaItunes/ArtistListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ArtistListViewController.swift
+//  PruebaItunes
+//
+//  Created by Andrés Murillas on 23/2/23.
+//
+
+import UIKit
+
+class ArtistListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PruebaItunes/ViewController.swift
+++ PruebaItunes/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let artistListViewController = ArtistListViewController(nibName: "ArtistListViewController", bundle: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func showArtistListView(_ sender: Any) {
+        
+        navigationController?.pushViewController(artistListViewController, animated: true)
+    }
+    
 
 }
 
