2bfa66ef77abd4e130c3ea134af167c7a1bc0909
andres murillas <andres.murillas@ciberexperis.es>
2023-03-06 09:29:47 +0100

    created ArtistDetailView and set navigation to it

---
+++ PruebaItunes/ArtistDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ArtistDetailViewController.swift
+//  PruebaItunes
+//
+//  Created by Andrés Murillas on 3/3/23.
+//
+
+import UIKit
+
+class ArtistDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
