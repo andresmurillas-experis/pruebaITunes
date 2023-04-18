7258f75de943428633f0e333e8280243d45bd0e0
andres murillas <andres.murillas@ciberexperis.es>
2023-04-18 12:23:48 +0200

    minor changes

--- PruebaItunes/ArtistList/Presenter/ArtistListPresenter.swift
+++ PruebaItunes/ArtistList/Presenter/ArtistListPresenter.swift
@@ -13,23 +13,22 @@ protocol ArtistListPresenterProtocol: AnyObject {
     func goToDetailViewForArtist(_ artist: ArtistModel)
 }
 
-struct ITunesAlbums {
-    var artist: ArtistModel?
-    var disc1: String?
-    var disc2: String?
-}
-
 final class ArtistListPresenter  {
     private var dataTask: URLSessionDataTask?
     weak var artistListView: ArtistListViewController?
     private var appDependencies: AppDependenciesResolver
     private var downloadClient: DownloadClient
-    private var iTunesAlbums: [ITunesAlbums?] = [] {
+    private var newArtistList: [ArtistModel] = [ArtistModel(id: 0, name: "")]
+    private var artistListNoDiscs: [ArtistModel] = [ArtistModel(id: 0, name: "")] {
         didSet {
-            artistList = insertCoversInto(artistList ?? [])
+            addDiscsToArtistIn(artistListNoDiscs)
         }
     }
-    private var artistList: [ArtistModel]?
+    private var artistList: [ArtistModel] = [ArtistModel(id: 0, name: "")] {
+        didSet {
+            self.artistListView?.setArtistList(artistList)
+        }
+    }
     private var albumList: [String?]?
     init(appDependencies: AppDependenciesResolver) {
         self.appDependencies = appDependencies
@@ -48,8 +47,7 @@ extension ArtistListPresenter: ArtistListPresenterProtocol {
         guard let artistQuery = artistListView?.searchText.replacingOccurrences(of: " ", with: "%20") else {
             return
         }
-        var albumList = [nil, nil, nil] as [AlbumModel?]
-        var artistList: [ArtistModel]?
+        var artistList: [ArtistModel] = [ArtistModel(id: 0, name: "")]
         downloadClient.download(from: "https://itunes.apple.com/search?term=\(artistQuery)&entity=musicArtist&attribute=artistTerm") { [weak self] (result: Result<ITunesArtistModel, DownloadClient.NetworkError>) in
             switch result {
             case .success(let iTunesArtistModel):
@@ -68,48 +66,40 @@ extension ArtistListPresenter: ArtistListPresenterProtocol {
                     print("JSON Parsing Error: ", error)
                 }
             }
-            self?.artistListView?.setArtistList(artistList)
+            self?.artistListNoDiscs = artistList
         }
     }
     func goToDetailViewForArtist(_ artist: ArtistModel) {
         coordinator.goToDetailViewForArtist(artist)
     }
-    
-    func insertCoversInto(_ artistList: [ArtistModel]) -> [ArtistModel] {
-        var newArtist: [ArtistModel] =  iTunesAlbums.map { album in
-            var artist = album?.artist
-            var disc1 = album?.disc1
-            var disc2 = album?.disc2
-            artist?.setDiscNames(discOneName: disc1, discTwoName: disc2)
-            return artist ?? ArtistModel(id: 0, name: "")
-        }
-        return newArtist
-    }
-    
-    func getAlbumCoversFor(_ artist: ArtistModel) {
-        downloadClient.download(from: "https://itunes.apple.com/lookup?id=\(artist.id)&entity=album&limit=2") { [weak self] (result: Result<ITunesAlbumModel, DownloadClient.NetworkError>) in
-            switch result {
-            case .success(let iTunesAlbumModel):
-                var discNames: [String?] = [nil, nil]
-                var albums: [ITunesAlbums] = []
-                discNames = iTunesAlbumModel.results.compactMap {
-                    if $0.collectionName == nil {
-                        return nil
+}
+private extension ArtistListPresenter {
+    func addDiscsToArtistIn(_ artistListNoDiscs: [ArtistModel]) {
+        var artistList: [ArtistModel] = [ArtistModel(id: 0, name: "")]
+        artistListNoDiscs.map { artist in
+            albumList = ["", ""]
+            self.downloadClient.download(from: "https://itunes.apple.com/lookup?id=\(artist.id)&entity=album") { [weak self] (result: Result<ITunesAlbumModel, DownloadClient.NetworkError>) in
+                switch result {
+                case .success(let iTunesAlbumModel):
+                    artistList.append(self?.createArtist(artist: artist, disc1: iTunesAlbumModel.results.first?.collectionName, disc2: iTunesAlbumModel.results.last?.collectionName) ?? ArtistModel(id: 0, name: ""))
+                case .failure(let error):
+                    switch error {
+                    case .serviceError:
+                        print("No Data Eroor: ", error)
+                    case .noData:
+                        print("Network Service Error: ", error)
+                    case .parsing:
+                        print("JSON Parsing Error: ", error)
                     }
-                    return $0.collectionName
-                }
-                albums.append(ITunesAlbums(artist: artist, disc1: discNames[0], disc2: discNames[1]))
-                self?.iTunesAlbums = albums
-            case .failure(let error):
-                switch error {
-                case .serviceError:
-                    print("No Data Eroor: ", error)
-                case .noData:
-                    print("Network Service Error: ", error)
-                case .parsing:
-                    print("JSON Parsing Error: ", error)
                 }
             }
         }
+        self.artistList = artistList
+    }
+    func createArtist(artist: ArtistModel, disc1: String?, disc2: String?) -> ArtistModel {
+        var artist = artist
+        artist.setDiscNames(discOneName: disc1, discTwoName: disc2)
+        return artist
+        
     }
 }
