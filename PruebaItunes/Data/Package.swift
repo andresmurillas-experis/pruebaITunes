45e4a01aef949a458cec9f06dd8d75535418ee2e
andres murillas <andres.murillas@ciberexperis.es>
2023-06-16 12:29:03 +0200

    minor fixes

--- PruebaItunes/Data/Package.swift
+++ PruebaItunes/Data/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .branch("master")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", branch: "master"),
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
